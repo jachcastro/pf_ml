d02d98b1d43533a91fc2c23ac64977cbb9b685e9
jachcastro <jachcastro@hotmail.com>
2023-10-11 18:43:57 -0500

    commit ml 11/10 0083

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
  
-st.write("""
-# Recomendacion de negocios 
-Somos *JL3*
-""")
- 
-
 # Cargar el archivo CSV
 @st.cache  # Usamos la caché de Streamlit para evitar la recarga constante del CSV
 def load_data():
@@ -19,8 +13,31 @@ st.title('Categoria ....')
 # Cargar los datos desde el archivo CSV
 data = load_data()
 
-# Widget ComboBox para seleccionar una categoría
-selected_category = st.selectbox("Selecciona una categoría:", data["categoria"])
+import streamlit as st
 
-# Mostrar la categoría seleccionada
-st.write("Categoría seleccionada:", selected_category)
+# Configuración del título
+st.title("JL3")
+
+# Crear un menú lateral
+st.sidebar.header("Alternativas")
+
+# Opciones del menú
+selected_option = st.sidebar.radio("Selecciona una opción", ["Inicio", "Para los Usuarios", "Para los Negocios","Para los Inversionistas" ])
+
+# Contenido principal basado en la opción seleccionada
+if selected_option == "Inicio":
+    st.write("""
+    # Proyecto de Recomendaciones  
+    ## Somos JL3
+    """)
+elif selected_option == "Para los Usuarios":
+    st.write("Aquí puedes configurar tus preferencias.")
+elif selected_option == "Para los Negocios":
+    st.write("Puedes obtener ayuda y soporte aquí.")
+elif selected_option == "Para los Inversionistas":
+    st.write("#Inversionistas")
+    # Widget ComboBox para seleccionar una categoría
+    selected_category = st.selectbox("Selecciona una categoría:", data["categoria"])
+
+    # Mostrar la categoría seleccionada
+    st.write("Categoría seleccionada:", selected_category)
