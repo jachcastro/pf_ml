4c6072a39fbd5a07821ba2477ee07359a8ae8cdc
jachcastro <jachcastro@hotmail.com>
2023-10-13 10:06:55 -0500

    commit ml 13/10 00023

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import inicio, usuarios, negocios, inversionistas, acercade
 import streamlit as st
+import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
 st.set_page_config(
