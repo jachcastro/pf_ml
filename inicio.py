5b819e669512d12acf7a4840a137090c18124ee4
jachcastro <jachcastro@hotmail.com>
2023-10-18 15:19:46 -0500

    commit ml 18/10 00047

--- inicio.py
+++ inicio.py
@@ -11,11 +11,12 @@ def app():
     # Cargar la imagen desde el archivo "mapa.png"
     imagen = "src/mapa.png"
 
-    # URL que deseas enlazar
+    # URL del video que deseas enlazar
+    video_url = "https://jl3.ntx360.net/mapa.mp4"
+
+    # Mostrar el video en la página
+    st.video(video_url)
+
+    # Enlace al mapa
     url = "https://jl3.ntx360.net/MapaML_Grupo07_01.html"
-
-    # Mostrar la imagen en el centro de la página
-    st.image(imagen, use_column_width=True)
-    # st.markdown("[MAPA]({url})")
     st.markdown("<div style='text-align: center;'><a href='{}' target='_blank'>MAPA</a></div>".format(url), unsafe_allow_html=True)
-
